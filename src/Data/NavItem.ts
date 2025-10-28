a9e67e37a66c563ff2483151c5aab8f3f73a38b0
Sohail Ansari <sohailansari483@gmail.com>
2025-10-28 13:03:27 +0530

    fix: paging, SEO erros and navigation

--- src/Data/NavItem.ts
+++ src/Data/NavItem.ts
@@ -1,10 +1,14 @@
-import { Trophy, BookMarked } from "lucide-react";
+import { Trophy } from "lucide-react";
 
-const navItems: Array<{ label: string; href: string; icon?: React.ElementType; }> = [
+export interface NavItemType{
+  label: string;
+  href: string;
+  icon?: React.ElementType;
+} 
+
+const navItems: NavItemType[] = [
   { label: "All Leagues", href: "/", icon: Trophy },
-  { label: "My Leagues", href: "/my-leagues", icon: BookMarked },
   { label: "FAQs", href: "/faqs", },
-  { label: "About", href: "/about" },
   {label: "Blogs", href: "/blogs"}
 ];
 
---
+++ src/Data/SocialHandles.ts
@@ -0,0 +1,6 @@
+const socialHandles: Array<{ label: string; href: string; icon?: string }> = [
+    {label: "Instagram", href: "https://www.instagram.com/nxtlap_races"},
+    {label: "LinkedIn", href: "http://linkedin.com/company/luxswipe"}
+];
+
+export default socialHandles;
